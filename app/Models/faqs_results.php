8063f9de83fa8db3ed691e718256fba1aec48ce5
a.kullab <alaa.kullab@irpal.ps>
2021-01-08 22:43:18 +0200

    creat faqs_results model and migration

---
+++ app/Models/faqs_results.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class faqs_results extends Model
+{
+    use HasFactory;
+}
