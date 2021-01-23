793344b80ec7161eecbe14b8b122b88e9f7abb2e
alaa.kullab <Alaa0592239632>
2021-01-23 21:44:43 +0200

    admin panel

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ Route::group(['prefix' => '{language}'], function () {
     //     abort(404);
     // }
 
-    Route::get('/', function () {
-        return view('welcome');
-    });
+    Route::get('admin', function () {
+        return view('admin.dashboard');
+    })->name('admin');
 
     Route::get('welcome', function () {
         return view('welcome');
