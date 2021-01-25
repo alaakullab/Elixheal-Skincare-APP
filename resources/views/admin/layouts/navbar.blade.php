78be15bdd1597b6484dab718026a98b48b1af1e0
a.kullab <alaa.kullab@irpal.ps>
2021-01-25 15:04:02 +0200

    c1

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -31,19 +31,67 @@
                             <i class="m-menu__link-bullet m-menu__link-bullet--dot">
                         <span></span></i><span class="m-menu__link-text">View questions</span></a>
                     </li>
-                    <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover">
-                        <a href="javascript:;" class="m-menu__link m-menu__toggle">
-                            <i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i>
-                            <span
-                                class="m-menu__link-text">Tabs</span>
-                                <i class="m-menu__ver-arrow la la-angle-right"></i></a>
-                        <div class="m-menu__submenu "><span class="m-menu__arrow"></span>
-                            <ul class="m-menu__subnav">
-                                <li class="m-menu__item " aria-haspopup="true"><a href="../../components/quiz/tabs/bootstrap.html" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">Bootstrap
-                                                    Tabs</span></a></li>
-                                <li class="m-menu__item " aria-haspopup="true"><a href="../../components/quiz/tabs/line.html" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">Line Tabs</span></a></li>
-                            </ul>
-                        </div>
+                </ul>
+            </div>
+        </li>
+
+        <li class="m-menu__item m-menu__item--expanded" aria-haspopup="true" m-menu-submenu-toggle="hover"><a href="javascript:;" class="m-menu__link m-menu__toggle"><i class="m-menu__link-icon flaticon-layers"></i><span
+            class="m-menu__link-text">Products</span>
+            <i class="m-menu__ver-arrow la la-angle-right"></i></a>
+            <div class="m-menu__submenu ">
+                <span class="m-menu__arrow"></span>
+                <ul class="m-menu__subnav">
+                    <li class="m-menu__item  m-menu__item--parent" aria-haspopup="true">
+                        <span class="m-menu__link">
+                            <span class="m-menu__link-text">Products</span></span>
+                    </li>
+                    <li class="m-menu__item " aria-haspopup="true">
+                        <a href="../../components/quiz/state.html" class="m-menu__link ">
+                            <i class="m-menu__link-bullet m-menu__link-bullet--dot">
+                        <span></span></i><span class="m-menu__link-text">View Products</span></a>
+                    </li>
+                    <li class="m-menu__item" aria-haspopup="true">
+                        <a href="../../components/quiz/state.html" class="m-menu__link ">
+                            <i class="m-menu__link-bullet m-menu__link-bullet--dot">
+                        <span></span></i><span class="m-menu__link-text">Add Product</span></a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+
+        <li class="m-menu__item m-menu__item--expanded" aria-haspopup="true" m-menu-submenu-toggle="hover"><a href="javascript:;" class="m-menu__link m-menu__toggle"><i class="m-menu__link-icon flaticon-layers"></i><span
+            class="m-menu__link-text">Orders</span>
+            <i class="m-menu__ver-arrow la la-angle-right"></i></a>
+            <div class="m-menu__submenu ">
+                <span class="m-menu__arrow"></span>
+                <ul class="m-menu__subnav">
+                    <li class="m-menu__item  m-menu__item--parent" aria-haspopup="true">
+                        <span class="m-menu__link">
+                            <span class="m-menu__link-text">Orders</span></span>
+                    </li>
+                    <li class="m-menu__item " aria-haspopup="true">
+                        <a href="../../components/quiz/state.html" class="m-menu__link ">
+                            <i class="m-menu__link-bullet m-menu__link-bullet--dot">
+                        <span></span></i><span class="m-menu__link-text">View Orders</span></a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+
+        <li class="m-menu__item m-menu__item--expanded" aria-haspopup="true" m-menu-submenu-toggle="hover"><a href="javascript:;" class="m-menu__link m-menu__toggle"><i class="m-menu__link-icon flaticon-layers"></i><span
+            class="m-menu__link-text">Orders</span>
+            <i class="m-menu__ver-arrow la la-angle-right"></i></a>
+            <div class="m-menu__submenu ">
+                <span class="m-menu__arrow"></span>
+                <ul class="m-menu__subnav">
+                    <li class="m-menu__item  m-menu__item--parent" aria-haspopup="true">
+                        <span class="m-menu__link">
+                            <span class="m-menu__link-text">Orders</span></span>
+                    </li>
+                    <li class="m-menu__item " aria-haspopup="true">
+                        <a href="../../components/quiz/state.html" class="m-menu__link ">
+                            <i class="m-menu__link-bullet m-menu__link-bullet--dot">
+                        <span></span></i><span class="m-menu__link-text">View Orders</span></a>
                     </li>
                 </ul>
             </div>
