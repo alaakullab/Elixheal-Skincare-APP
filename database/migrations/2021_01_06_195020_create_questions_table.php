62d6de6b3e407298a2e4d94794e0211e93d70d5e
a.kullab <alaa.kullab@irpal.ps>
2021-02-05 16:44:58 +0200

    language_id change table questions

--- database/migrations/2021_01_06_195020_create_questions_table.php
+++ database/migrations/2021_01_06_195020_create_questions_table.php
@@ -16,7 +16,6 @@ class CreateQuestionsTable extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->integer('language_id')->unsigned()->nullable()->comment('Language Number To Choose a display language from table const_languages');
-            $table->foreign('language_id')->references('id')->on('const_languages')->onDelete('cascade');
             $table->string('question_value')->comment('To add const question value (not result) ');
             $table->string('question_type')->comment('To determine the type of question');
             $table->integer('user_id')->unsigned()->nullable()->comment('User Id from table users');
