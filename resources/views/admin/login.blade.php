20681901d2ecf6591a84411b08c7298a51716bf3
alaa.kullab <Alaa0592239632>
2021-02-09 15:41:14 +0200

    login page

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -99,12 +99,22 @@ License: You must have a valid license purchased only from themeforest(the above
 							<div class="m-login__head">
 								<h3 class="m-login__title">Sign In To Admin</h3>
 							</div>
-							<form class="m-login__form m-form" action="">
+							<form class="m-login__form m-form" method="POST" action="{{ route('admin.login', app()->getLocale()) }}">
 								<div class="form-group m-form__group">
-									<input class="form-control m-input" type="text" placeholder="Email" name="email" autocomplete="off">
-								</div>
+                                    <input class="form-control m-input @error('email') is-invalid @enderror" type="email" placeholder="Email" id="email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus >
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
 								<div class="form-group m-form__group">
-									<input class="form-control m-input m-login__form-input--last" type="password" placeholder="Password" name="password">
+									<input class="form-control m-input m-login__form-input--last @error('password') is-invalid @enderror" id="password" type="password" placeholder="Password" name="password">
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
 								</div>
 								<div class="row m-login__form-sub">
 									<div class="col m--align-left m-login__form-left">
@@ -118,8 +128,13 @@ License: You must have a valid license purchased only from themeforest(the above
 									</div>
 								</div>
 								<div class="m-login__form-action">
-									<button id="m_login_signin_submit" class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn m-login__btn--primary">Sign In</button>
-								</div>
+									<button type="submit" class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn m-login__btn--primary"> {{ __('Login') }}</button>
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif
+                                </div>
 							</form>
 						</div>
 {{--						<div class="m-login__signup">--}}
