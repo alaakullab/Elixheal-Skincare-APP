253890a5bbabafaecad7ad19168dab4218dba96f
a.kullab <alaa.kullab@irpal.ps>
2021-02-13 11:21:08 +0200

    App\Models\  fix bug Class 'App\Models\setting' not found in heroku

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,16 +1,16 @@
 <?php
 
 namespace Database\Seeders;
-use App\Models\User;
-use App\Models\Category;
-use App\Models\question;
-use App\Models\answer;
-use App\Models\const_languages;
-use App\Models\contact;
-use App\Models\product;
-use App\Models\faqs_question;
-use App\Models\faqs_answer;
-use App\Models\setting;
+// use App\Models\User;
+// use App\Models\Category;
+// use App\Models\question;
+// use App\Models\answer;
+// use App\Models\const_languages;
+// use App\Models\contact;
+// use App\Models\product;
+// use App\Models\faqs_question;
+// use App\Models\faqs_answer;
+// use App\Models\setting;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -25,15 +25,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->create();
-        const_languages::factory(4)->create();
-        // Category::factory(5)->create();
-        // contact::factory(10)->create();
-        // product::factory(6)->create();
-        // question::factory(10)->create();
-        // answer::factory(30)->create();
-        // faqs_question::factory(30)->create();
-        // faqs_answer::factory(90)->create();
-        setting::factory(4)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\const_languages::factory(4)->create();
+        \App\Models\Category::factory(5)->create();
+        \App\Models\contact::factory(10)->create();
+        \App\Models\product::factory(6)->create();
+        \App\Models\question::factory(10)->create();
+        \App\Models\answer::factory(30)->create();
+        \App\Models\faqs_question::factory(30)->create();
+        \App\Models\faqs_answer::factory(90)->create();
+        \App\Models\setting::factory(4)->create();
     }
 }
