942ed71ee58cb33f50e2d809a7797a7d63f3597b
alaa.kullab <Alaa0592239632>
2021-03-05 22:38:49 +0200

    front page - update quiz

--- resources/lang/en/front.php
+++ resources/lang/en/front.php
@@ -7,6 +7,10 @@ return array(
     'about_us' => 'About US',
     'start_skin_quiz' => 'START SKIN QUIZ',
 
+    //quiz
+    'quiz_title_formulate_your_custom_skincare' => "Formulate your custom skincare",
+    'quiz_dec_3_minutes_to_your_best_skincare_let_is_go' => "3 minutes to your best skincare, let's go!",
+
 )
 
 ?>
